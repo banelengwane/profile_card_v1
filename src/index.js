f1a9d22b67827d8595dd56456a911cc76f7eafc7
banelengwane <banelengwane@gmail.com>
2023-12-24 09:59:25 +0200

    Profile card finished with relevant props

--- src/index.js
+++ src/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export default function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="avatar.jpg" alt="Banele Ngwane" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Banele Ngwane</h1>
+      <p>
+        Full-Stack web developer. When not coding, I conduct a choral choir and
+        help kids with their math problems (tutor).
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="😀" color="blue" />
+      <Skill skill="HTML+CSS" emoji="😀" color="orangered" />
+      <Skill skill="Svelte" emoji="😀" color="yellow" />
+      <Skill skill="Javascript" emoji="😀" color="green" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
